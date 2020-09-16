ecb763c39af6c8da4f64c0a14a53951d3b72b3d0
johnasut <john.saunders@pharm.utah.edu>
2020-09-15 20:14:22 -0600

    Create Python .py files assignment

---
+++ exerPythCreateBasic/calc.py
@@ -0,0 +1,8 @@
+x=int(input('What is the first number? '))
+y=int(input('What is the second number? '))
+result=(x+y)
+print(x,'+',y,'=',result)
+
+print('or')
+
+print(str(x)+' + '+str(y)+' = '+str(result))
